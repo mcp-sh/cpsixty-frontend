e31716efd2921846eeb77abf5476ac1f627658f3
Office - MCP <mcp@macapa.me>
2021-06-02 09:26:05 +0800

    Implemented Token Auth

--- src/services/GuestService.js
+++ src/services/GuestService.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://cpsixty-api.herokuapp.com/api",
-  // baseURL: "http://localhost:3000/api",
+  // baseURL: "https://cpsixty-api.herokuapp.com/api",
+  baseURL: "http://localhost:3000/api",
   withCredentials: false,
   headers: {
+    "API-Token":
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjp0cnVlLCJpYXQiOjE2MjI1OTQ3Nzh9.tMF6WMNREGrHlYrpIlK0MCEsisZantq4bwdhxcUypGM",
     Accept: "application/json",
     "Content-Type": "application/json",
   },
